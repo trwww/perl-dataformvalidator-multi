b50a77861e1620f38d6bd0bfc09cc5538fd74250
Todd Wade <waveright@gmail.com>
2017-01-26 02:44:34 -0500

    modify the profile from the base/skeleton profile - comment out of base data to verify

--- t/lib/Test/Data/FormValidator/Multi/Nested.pm
+++ t/lib/Test/Data/FormValidator/Multi/Nested.pm
@@ -6,12 +6,20 @@ package # Hide from PAUSE
 
 use base qw(Test::Data::FormValidator::Multi);
 use Test::More;
+use Data::Dumper;
 
-sub basic : Test(2) {
+sub nested : Test(2) {
   my $self = shift;
 
   my $data = $self->skeleton_data;
-  my $dfv  = $self->skeleton_validator;
+  my $dfv  = $self->nested_validator;
+
+# $data->{hash_in_hash}{foo}{bar} = $self->timezones(
+#    [ 999, 'America/New_York',    'Home',  '01/01', '23:59'],
+#    [ 111, 'America/Los_Angeles', 'L. A.', '01/01', '20:59'],
+#  );
+
+  diag( Data::Dumper->Dump([$data], ['data']) );
 
   isa_ok(
     my $results = $self->{results} = $dfv->check($data)
@@ -20,6 +28,20 @@ sub basic : Test(2) {
   );
 
   ok(! $results->success, 'data is invalid');
+}
+
+sub nested_validator {
+  my $self = shift;
+
+  my $profile = $self->main_profile;
+  $profile->add( 'hash_in_hash',
+    required => 1,
+  );
+
+  my $dfv = $self->skeleton_validator;
+  $dfv->{profiles}{profile} = $profile->profile;
+
+  return $dfv;
 }
 
 1;
