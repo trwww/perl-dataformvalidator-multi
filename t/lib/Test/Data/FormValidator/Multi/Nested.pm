ac5d727b5b4a27b05981cfa6356283b5b22839cd
Todd Wade <waveright@gmail.com>
2017-01-26 02:11:14 -0500

    a copy of Test::Data::FormValidator::Multi::Nested w/ updated package name

---
+++ t/lib/Test/Data/FormValidator/Multi/Nested.pm
@@ -0,0 +1,25 @@
+use warnings;
+use strict;
+
+package # Hide from PAUSE
+  Test::Data::FormValidator::Multi::Nested;
+
+use base qw(Test::Data::FormValidator::Multi);
+use Test::More;
+
+sub basic : Test(2) {
+  my $self = shift;
+
+  my $data = $self->skeleton_data;
+  my $dfv  = $self->skeleton_validator;
+
+  isa_ok(
+    my $results = $self->{results} = $dfv->check($data)
+      =>
+    'Data::FormValidator::Results' => '$results'
+  );
+
+  ok(! $results->success, 'data is invalid');
+}
+
+1;
